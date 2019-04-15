61516983a7c243e26ce6b67de9dfd0337a2ca72f
Fuzoka <unbeatablesites09@gmail.com>
2019-04-14 23:54:05 -0400

    Disable Default

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -6,7 +6,7 @@ class SearchBar extends React.Component {
   render() {
     return (
       <div className="ui segment">
-        <form onSubmit={} className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label> Image Search</label>
             <input
