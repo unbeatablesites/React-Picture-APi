a6fee44ab1a1a029391cc8b43c05e7656f256db0
Fuzoka <unbeatablesites09@gmail.com>
2019-04-14 23:55:28 -0400

    Prevent default

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -3,6 +3,10 @@ import React from "react";
 class SearchBar extends React.Component {
   state = { term: "" };
 
+  onFormSubmit() {
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div className="ui segment">
