ec1f208630081f06334194234229067c0e77d472
Fuzoka <unbeatablesites09@gmail.com>
2019-04-18 20:41:38 -0400

    Invoking Callbacks in Children

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -5,7 +5,7 @@ class SearchBar extends React.Component {
 
   onFormSubmit = event => {
     event.preventDefault();
-    this.props.onSubmit;
+    this.props.onSubmit(this.state.term);
   };
 
   render() {
