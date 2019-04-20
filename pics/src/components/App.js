8e105f46f1f40070ad3f83f7dd91ca952d46dc60
Fuzoka <unbeatablesites09@gmail.com>
2019-04-19 20:45:00 -0400

    add Params

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -6,9 +6,11 @@ class App extends React.Component {
   onSearchSubmit(term) {
     // console.log(term);
     axios.get("https://api.unsplash.com/search/photos", {
-        headers;{
-            Authorization: "Client-ID 0286e3369a6897edef448e5f73cf2c3054447ffb44b06a3c0a84b2d77e9ae70b"
-        }
+      params: { query: term },
+      headers: {
+        Authorization:
+          "Client-ID 0286e3369a6897edef448e5f73cf2c3054447ffb44b06a3c0a84b2d77e9ae70b"
+      }
     });
   }
   render() {
